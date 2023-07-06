b8c02eac727bc17ac52ba71940eaca7b6e58208c
Fombi-Favour <fombifavour@gmail.com>
2023-07-06 13:33:23 +0100

    Update Quote.js

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import 'Quote.css';
+import './Quote.css';
 
 const Quote = () => {
   const url = 'https://api.api-ninjas.com/v1/quotes?category=learning';
