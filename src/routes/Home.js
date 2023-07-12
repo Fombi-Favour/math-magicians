a1efbf8c13e5895ec01482d53f12b32dc8a9aa97
Fombi-Favour <fombifavour@gmail.com>
2023-07-12 15:04:29 +0100

    Update Home.js

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,7 +1,32 @@
 import React from 'react';
+import '../styles/App.css';
 
 const Home = () => (
-  <div>Home Section</div>
+  <div className="wrapper">
+    <h2>Welcome to our page!</h2>
+    <p>
+      Math magicians is a website for all fans of mathematics. It is a Single
+      Page App (SPA) that allows users to make simple calculations and read a
+      random math-related quote.
+    </p>
+    <p>
+      Mathematics is an area of knowledge that includes the topics of numbers,
+      formulas and related structures, shapes and the spaces in which they are
+      contained, and quantities and their changes. These topics are represented
+      in modern mathematics with the major subdisciplines of number theory,
+      algebra, geometry, and analysis, respectively. There is no
+      general consensus among mathematicians about a common definition for their
+      academic discipline. Most mathematical activity involves the discovery of
+      properties of abstract objects and the use of pure reason to prove them.
+      These objects consist of either abstractions from nature or—in modern
+      mathematics—entities that are stipulated to have certain properties,
+      called axioms. A proof consists of a succession of applications of
+      deductive rules to already established results. These results include
+      previously proved theorems, axioms, and—in case of abstraction from
+      nature—some basic properties that are considered true starting points of
+      the theory under consideration.
+    </p>
+  </div>
 );
 
 export default Home;
