139fdf4b6cd67be90ed71a75d8bd4ebf52c36f33
Fombi-Favour <fombifavour@gmail.com>
2023-07-12 14:02:47 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import './App.css';
-import Calculator from './components/Calculator';
-import Quote from './components/Quote';
+import './styles/App.css';
+import { Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
+import Home from './routes/Home';
+import Calculator from './routes/Calculator';
+import Quote from './routes/Quote';
 
 function App() {
   return (
-    <div className="sections">
-      <Quote />
-      <Calculator />
-    </div>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="calculator" element={<Calculator />} />
+        <Route path="quote" element={<Quote />} />
+      </Route>
+    </Routes>
   );
 }
 
